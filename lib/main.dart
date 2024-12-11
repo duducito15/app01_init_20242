8fbf2045edf3f9b80e985700f15f52039ad9cf5d
Eduardo <echavezv@utea.edu.pe>
2024-12-11 07:02:36 -0500

    Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,40 @@ void main(List<String> args) {
                   fit: BoxFit.fill,
                 ),
               ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  int numero = 100;
+                  print("Hola a todos!!! $numero");
+                },
+                child: const Text("Click!!!"),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: Text("Otro ElevatedButton"),
+                icon: Icon(
+                  Icons.access_alarm_rounded,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  print("Click, desde el Floating Action Button");
+                },
+                child: const Icon(
+                  Icons.plus_one,
+                ),
+              ),
+              FilledButton(
+                onPressed: () {},
+                child: Text("Otro Click!!!"),
+              ),
+              FilledButton.icon(
+                onPressed: () {},
+                label: Text("Otro FilledButton"),
+                icon: Icon(Icons.accessibility_new),
+              ),
             ],
           ),
         ),
